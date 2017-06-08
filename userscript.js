ea2d156535ab744b1f714f663db4518bbbbee6a4
Manuel Dionne <mandio0302@gmail.com>
2017-06-08 12:12:20 -0400

    Added avatars and links

--- userscript.js
+++ userscript.js
@@ -12,6 +12,7 @@
 (function() {
     'use strict';
 
+    //helper functions
     function nthIndex(str, pat, n){
         var L= str.length, i= -1;
         while(n-- && i++<L){
@@ -35,11 +36,11 @@
         while(nthIndex(content,"Quote",count) != -1){
             count++;
         }
-        alert(count);
+        //alert(count);
         if(content.indexOf("Quote") != -1)
         {
             while((opentag || content.indexOf("Quote", position)) != -1 && (opentag || first)) {
-                alert(opentag + "--"+j+"--" + first + "  :  " + position);
+                //alert(opentag + "--"+j+"--" + first + "  :  " + position);
                 if(first)
                 {
                     var after= nthIndex(content,"Quote",j);
@@ -61,5 +62,68 @@
         $("p").eq(i).html(content);
     }
 
-    //for(var i = 0; i < $("")
+    //make naked links <a> links
+    var urlRegex = /\b(https?:\/\/[^\s+\"\<\>]+)/ig;
+	var snapTextElements = document.evaluate("//text()[not(ancestor::a) " + "and not(ancestor::script) and not(ancestor::style) and " + "contains(translate(., 'HTTP', 'http'), 'http')]", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+	for (var x = snapTextElements.snapshotLength - 1; x >= 0; x--) {
+		var elmText = snapTextElements.snapshotItem(x);
+		if (urlRegex.test(elmText.nodeValue)) {
+			var elmSpan = document.createElement("span");
+			var sURLText = elmText.nodeValue;
+			elmText.parentNode.replaceChild(elmSpan, elmText);
+			urlRegex.lastIndex = 0;
+			for (var match = null, lastLastIndex = 0;(match = urlRegex.exec(sURLText)); ) {
+				elmSpan.appendChild(document.createTextNode(sURLText.substring(lastLastIndex, match.index)));
+				var elmLink = document.createElement("a");
+				elmLink.setAttribute("href", match[0]);
+				elmLink.appendChild(document.createTextNode(match[0]));
+				elmSpan.appendChild(elmLink);
+				lastLastIndex = urlRegex.lastIndex;
+			}
+			elmSpan.appendChild(document.createTextNode(sURLText.substring(lastLastIndex)));
+			elmSpan.normalize();
+		}
+	}
+
+    //avatars n shiet
+    //-5 to the orginal link because ur taking out the ;wap2
+    var users = {}, avatarformats = {};
+    $.get(window.location.href.substring(0, window.location.href.length - 5), function(data, status){
+        //if(status != 200) {
+        //    alert("Data: " + data + "\nStatus: " + status);
+        //}
+        var wrapper= document.createElement('div');
+        wrapper.innerHTML= data;
+        //alert(wrapper.innerHTML);
+
+        var links = wrapper.getElementsByTagName('a');
+        for(var i = 0; i< links.length; i++){
+            if(/u\=(.*?)$/.test(links[i].href))
+               users[links[i].innerHTML] = links[i].href.substring(links[i].href.indexOf("u=")+2);
+        }
+        var avatarlinks = wrapper.getElementsByTagName('img');
+        for(var j = 0; j< avatarlinks.length; j++){
+            if(/avatar_(.*?)$/.test(avatarlinks[j].src))
+            {
+               var format = avatarlinks[j].src.slice(-4);
+               var user = avatarlinks[j].src.substring(avatarlinks[j].src.indexOf("avatar_")+7,avatarlinks[j].src.indexOf(format));
+               avatarformats[user] = format;
+            }
+        }
+        console.log(JSON.stringify(avatarformats));
+
+        //insert images
+        for(var y = 0; y < $("b").length; y++)
+        {
+            var userid =  users[$("b").eq(y).html()];
+            var avatar = new Image(15, 15);
+            avatar.src = 'https://forum.blockland.us/avatarUpload/avatar_' + userid + avatarformats[userid];
+            avatar.userid = userid;
+            if(typeof userid !== undefined)
+            {
+                $("b").eq(y).html( avatar.outerHTML + $("b").eq(y).html());
+            }
+        }
+    });
+
 })();
